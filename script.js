d6963b0eeabcb814c5e2de2d8c94f7b71dc90f43
andzia0520 <ankadaniluk@gmail.com>
2021-01-28 17:55:08 +0100

    http

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://lit-caverns-05126.herokuapp.com/v1/';
-  const trelloApiRoot = 'https://lit-caverns-05126.herokuapp.com/v1/trello/';
+  const apiRoot = 'http://lit-caverns-05126.herokuapp.com/v1/';
+  const trelloApiRoot = 'http://lit-caverns-05126.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
